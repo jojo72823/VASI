3b3318d7038ee0599c09299bfeedebbbc0f23324
jonathan daumont <jojo-d72@hotmail.fr>
2017-10-13 09:13:17 +0200

    correction de bug mineur

--- app/src/main/java/com/daumont/vasi/vasi/activity/ActivityLogin.java
+++ app/src/main/java/com/daumont/vasi/vasi/activity/ActivityLogin.java
@@ -52,7 +52,6 @@ public class ActivityLogin extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         activity = this;
 
         //Recuperation des elements visuels
